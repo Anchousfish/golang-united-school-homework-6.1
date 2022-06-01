1d1631bc448d8aa82c77125c1ba56fa2f24b4ad5
Anchousfish <anchousfish@gmail.com>
2022-06-01 14:23:21 +0300

    v.1

--- box.go
+++ box.go
@@ -1,7 +1,7 @@
 package golang_united_school_homework
 
+import "errors"
 
-// box contains list of shapes and able to perform operations on them
 type box struct {
 	shapes         []Shape
 	shapesCapacity int // Maximum quantity of shapes that can be inside the box.
@@ -11,51 +11,97 @@ type box struct {
 func NewBox(shapesCapacity int) *box {
 	return &box{
 		shapesCapacity: shapesCapacity,
+		shapes:         make([]Shape, 0),
 	}
 }
 
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	panic("implement me")
+	if len(b.shapes) < b.shapesCapacity {
+		b.shapes = append(b.shapes, shape)
+		return nil
+	} else {
+		return errors.New("can't add a shape, the box is full ")
+	}
+
 }
 
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	panic("implement me")
-
+	if b.shapesCapacity >= (i+1) && len(b.shapes) >= i+1 {
+		return b.shapes[i], nil
+	} else {
+		return nil, errors.New("no shape found")
+	}
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	panic("implement me")
+
+	if b.shapesCapacity >= (i+1) && len(b.shapes) >= i+1 {
+		v := b.shapes[i]
+		for ; i < len(b.shapes); i++ {
+			b.shapes[i] = b.shapes[i+1]
+		}
+		return v, nil
+	} else {
+
+		return nil, errors.New("no box found")
+	}
 
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	panic("implement me")
+	if b.shapesCapacity >= (i+1) && len(b.shapes) >= i+1 {
+		v := b.shapes[i]
+		b.shapes[i] = shape
+		return v, nil
+	} else {
+
+		return nil, errors.New("nothing to replace")
+	}
 
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	panic("implement me")
-
+	var result float64
+	for _, shape := range b.shapes {
+		result += shape.CalcPerimeter()
+	}
+	return result
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	panic("implement me")
+	var result float64
+	for _, shape := range b.shapes {
+		result += shape.CalcArea()
+	}
+	return result
 
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	panic("implement me")
+	j := 0
+	for i := range b.shapes {
+		if _, ok := b.shapes[i].(*Circle); ok {
+			for j = i; j < len(b.shapes); j++ {
+				b.shapes[j] = b.shapes[j+1]
+			}
+		}
+	}
+	if j == 0 {
+		return errors.New("no circles in the list")
+	} else {
+		return nil
+	}
 
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Anchousfish/golang-united-school-homework-6.1
+
+go 1.18
